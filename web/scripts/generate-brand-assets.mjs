70dbe03536e20e5863feeb695211d57259f0d79f
BP <bhavikpareek98@gmail.com>
2026-09-23 21:36:09 +0530

    fix: preserve final newline in WeCloud wordmark

--- web/scripts/generate-brand-assets.mjs
+++ web/scripts/generate-brand-assets.mjs
@@ -40,7 +40,7 @@ const lockupWidth = Math.ceil(110 + cursor * wordScale)
 const lockupSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${lockupWidth} 180">
   <g transform="translate(0 20) scale(.6)">${mark}</g>
   <g fill="#fff" transform="translate(110 130) scale(${wordScale} -${wordScale})">${glyphs}</g>
-</svg>`
+</svg>\n`
 
 await mkdir(publicDirectory, { recursive: true })
 await writeFile(path.join(publicDirectory, 'brand-lockup.svg'), lockupSvg)
